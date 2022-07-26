608762fffba5f4b2541591a587523e063dce8ed1
Meron Ambelu <meronambelu@Merons-MacBook-Air.local>
2022-07-26 12:26:40 -0400

    .properties

---
+++ meron-ambelu-train-reservation/src/main/java/com/example/meronambelutrainreservation/MeronAmbeluTrainReservationApplication.java
@@ -0,0 +1,14 @@
+package com.example.meronambelutrainreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@
+public class MeronAmbeluTrainReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeronAmbeluTrainReservationApplication.class, args);
+	}
+
+}
---
+++ meron-ambelu-train-reservation/src/test/java/com/example/meronambelutrainreservation/MeronAmbeluTrainReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.meronambelutrainreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeronAmbeluTrainReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
